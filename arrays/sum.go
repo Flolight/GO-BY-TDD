2612046be36092adf42a813bda3bd479b5b154ce
FlorianClanet <florianclanet@gmail.com>
2020-10-14 01:05:18 +0200

    feat|add some slice exploration

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,9 +1,31 @@
-package main
+package sum
 
+// Sum numbers
 func Sum(numbers []int) int {
 	sum := 0
 	for _, number := range numbers {
 		sum += number
 	}
 	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	// lenghtOfNumbers := len(numbersToSum)
+	// sums := make([]int, lenghtOfNumbers)
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
+}
+
+// Sum independently each slice and returns a slice with each result
+func SumAllTails(numbersToSum ...[]int) []int {
+	var slices [][]int
+	for _, slice := range numbersToSum {
+		tail := slice[1:]
+		slices = append(slices, tail)
+	}
+	// Spread operator to unroll the slice of slices
+	return SumAll(slices...)
 }
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,6 +1,10 @@
-package main
+package sum
 
-import "testing"
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
@@ -15,15 +19,28 @@ func TestSum(t *testing.T) {
 			t.Errorf("got '%d' but want '%d' given: %v", got, want, numbers)
 		}
 	})
+}
 
-	// t.Run("collection of any size", func(t *testing.T) {
-	// 	numbers := []int{1, 2, 3}
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
 
-	// 	got := Sum(numbers)
-	// 	want := 6
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v but want %v", got, want)
+	}
+}
 
-	// 	if got != want {
-	// 		t.Errorf("got '%d' but want '%d' given: %v", got, want, numbers)
-	// 	}
-	// })
+func TestSumAllTails(t *testing.T) {
+	got := SumAllTails([]int{1, 2}, []int{0, 9})
+	want := []int{2, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v but want %v", got, want)
+	}
+}
+
+func ExampleSumAllTails() {
+	sums := SumAllTails([]int{1, 2, 3}, []int{0, 9, 10})
+	fmt.Println(sums)
+	// Output: [5 19]
 }
