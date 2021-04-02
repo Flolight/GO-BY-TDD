56b5351e207df335dc018e645e9eda96f6c6a73a
FlorianClanet <florianclanet@gmail.com>
2021-04-02 23:45:51 +0200

    feat|improve sleep test by checking order

--- mockup/countdown.go
+++ mockup/countdown.go
@@ -7,19 +7,11 @@ import (
 	"time"
 )
 
-const FinalWord = "Go!"
-const CountdownStart = 3
+const finalWord = "Go!"
+const countdownStart = 3
 
 type Sleeper interface {
 	Sleep()
-}
-
-type SpySleeper struct {
-	Calls int
-}
-
-func (s *SpySleeper) Sleep() {
-	s.Calls++
 }
 
 type DefaultSleeper struct{}
@@ -28,13 +20,17 @@ func (d *DefaultSleeper) Sleep() {
 	time.Sleep(1 * time.Second)
 }
 
+const write = "write"
+const sleep = "sleep"
+
 func Countdown(out io.Writer, sleeper Sleeper) {
-	for i := CountdownStart; i > 0; i-- {
+	for i := countdownStart; i > 0; i-- {
 		sleeper.Sleep()
 		fmt.Fprintln(out, i)
 	}
+
 	sleeper.Sleep()
-	fmt.Fprint(out, FinalWord)
+	fmt.Fprint(out, finalWord)
 }
 
 func main() {
